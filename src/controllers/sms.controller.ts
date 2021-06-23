09e719b885d7629fc83aae93204c8d7ea26aac40
Manuel Schüpbach <manuel.schuepbach1@students.unibe.ch>
2021-06-23 10:47:29 +0200

    send test sms

--- src/controllers/sms.controller.ts
+++ src/controllers/sms.controller.ts
@@ -28,7 +28,7 @@ export class SmsController implements ControllerFactory {
             this._serialCommander.write('AT+CMGF=1\n');
             this._serialCommander.write('AT+CSMP=17,167,0,144\n');
             this._serialCommander.write('AT+CMGS="0786447590"\n');
-            this._serialCommander.write('Hallo from API\n');
+            this._serialCommander.write('Hallo from API\u001A');
             this._serialCommander.on('readable', (data) => {
                 console.log('modem: ', this._serialCommander.read().toString());
             });
