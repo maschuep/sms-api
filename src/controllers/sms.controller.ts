662b1b35947cb624e5e69a35e5456be4f461c0ec
Manuel Schüpbach <manuel.schuepbach1@students.unibe.ch>
2021-06-23 10:47:25 +0200

    send test sms

--- src/controllers/sms.controller.ts
+++ src/controllers/sms.controller.ts
@@ -24,7 +24,11 @@ export class SmsController implements ControllerFactory {
     sendSms() {
         this._router.get('/send', (req: Request, res: Response) => {
             console.log('sending AT');
-            this._serialCommander.write('AT\r', (str: Error) => { console.log('sent AT'); res.status(200).send(str); });
+            this._serialCommander.write('AT\n', (err: Error) => { console.log('sent AT'); res.status(200).send({error: err}); });
+            this._serialCommander.write('AT+CMGF=1\n');
+            this._serialCommander.write('AT+CSMP=17,167,0,144\n');
+            this._serialCommander.write('AT+CMGS="0786447590"\n');
+            this._serialCommander.write('Hallo from API\n');
             this._serialCommander.on('readable', (data) => {
                 console.log('modem: ', this._serialCommander.read().toString());
             });
